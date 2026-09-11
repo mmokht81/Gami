32e7822ce2851c36758a60a9f315a6289a7c98dd
mahdieh <mahdiehmokht@gmail.com>
2026-09-11 21:03:40 +0330

    fix token error

--- accounts/token_serializers.py
+++ accounts/token_serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.exceptions import AuthenticationFailed
+
 from .models import User
+
 
 class GamiTokenObtainPairSerializer(TokenObtainPairSerializer):
 
-    def validate(self, attrs):
+    default_error_messages = {
+        **TokenObtainPairSerializer.default_error_messages,
+        "no_active_account": "شماره موبایل یا رمز عبور صحیح نیست.",
+    }
 
+    def validate(self, attrs):
         phone_number = attrs.get(User.USERNAME_FIELD)
 
         user = User.objects.filter(
@@ -20,12 +26,9 @@ class GamiTokenObtainPairSerializer(TokenObtainPairSerializer):
             )
 
         try:
-            data = super().validate(attrs)
+            return super().validate(attrs)
 
         except AuthenticationFailed:
             raise serializers.ValidationError(
                 "شماره موبایل یا رمز عبور صحیح نیست."
             )
-
-        return data
-
